6753fdc22164c76b5b8e7f732c6f8add9bf3e999
Dipesh Malla <gamerdipesh002@gmail.com>
2026-08-10 18:12:00 +0545

    Add: getCommentsByBlog route

--- Day7- Blog Project/src/features/comment/comment.routes.js
+++ Day7- Blog Project/src/features/comment/comment.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createComment } from "./comment.controllers.js";
+import { createComment, getCommentsByBLog } from "./comment.controllers.js";
 import { VerifyJWT } from "../../middleware/verifyJWT.js";
 
 const commentRouter = express.Router();
 
 commentRouter.post("/:blogId/comments", VerifyJWT, createComment);
-
+commentRouter.get("/:blogId/comments", getCommentsByBLog);
 
 export default commentRouter;
